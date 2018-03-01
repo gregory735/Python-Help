e304edb8356f60a719f85e7afd269c86075f0d94
gregory735 <gregory_735@hotmail.com>
2018-03-01 13:39:23 -0300

    Adicionando arquivos função em uma pasta mudando alguns nomes dos arquivos e comentando no arquivo menu de areas

--- Programas com Função/Menu de areas função.py
+++ Programas com Função/Menu de areas função.py
@@ -5,7 +5,7 @@ Spyder Editor
 This is a temporary script file.
 """
 
-#--------------- função Menu de verificação-------------
+#%%--------------- função Menu de verificação-------------
 def menu ():
     print('\033[34m=-=-=-=-=-=-=-=-=-=-\033[m \033[4;31mMenu de opções\033[m \033[34m=-=-=-=-=-=-=-=-=-=-==-=\033[m')
     print("")
@@ -14,9 +14,9 @@ def menu ():
     print("\033[34mCalcular a Área do Quadrado....... Digite:\033[m   \033[1;31m1\033[m")
     print("\033[34mCalcular a Área do Triangulo........ Digite:\033[m   \033[1;31m2\033[m")
     print("\033[34mCalcule a Distancia de dois pontos....Digite:\033[m   \033[1;31m3\033[m")
-    return
+    return # <---------- esse return não precisaria existir
     
-#-----------------------------
+#%%----------------------------- Função que calcula area do quadrado--------------------------------
 def quadrado():
     escolha = 1
     base = float(input('Digite a base do quadrado: '))
@@ -24,7 +24,8 @@ def quadrado():
     area = base * altura
     printar(escolha,area)
     return area
-    
+
+#%%--------------------------------- Função que calcula área do triangulo ----------------
 def triangulo():
     escolha = 2
     base = float(input('Digite a base do triangulo: '))
@@ -32,7 +33,9 @@ def triangulo():
     area = base * altura / 2
     printar(escolha,area)
     return area
-    
+
+
+#%%--------------------------------- Distancia entre 2 pontos ---------------------------
 def distanciaPontos():
     escolha = 3
     p1 = float(input('Digite o valor em metros onde o ponto 1 está: '))
@@ -41,17 +44,21 @@ def distanciaPontos():
         distancia = p1 - p2
     else:
         distancia = p2 - p1
-    printar(escolha,distancia)
+    printar(escolha,distancia) # ---------------- Utilizando a função printar e receberá o valor definido aqui ----------------
     return distancia
 
-def printar(escolha,saida):
+
+#%%--------------------------------Função para printar os resultados --------------------
+def printar(escolha,saida): #--------------------- Escolha e saida vai ser definido dependendo do que você quiser printar----------------
     if escolha == 1:
         print('A area do quadrado é:',saida)
     elif escolha == 2: 
         print('A area do triangulo é:',saida)
     elif escolha == 3:
         print('A distancia dos pontos é:',saida)
-        
+
+
+#%%------------------------------------ Programa Principal verificação de entrada ---------------------------------
 entrada = 1
 while entrada != 0:
     entrada = int(input('Digite \033[31mqualquer\033[m número para abrir o menu ou Digite \033[31m0\033[m para fechar o programa:'))
